36df98fee2ddd43785168c119e758af985feaa39
laxmanprometteur <lnemane7@gmail.com>
2022-11-06 18:06:01 +0530

    folder structure and button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "./Component/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  return <div className="App">
+    <Button text="Click" oK={() => alert("Click")} />
+    <Button text="me" oK={() => alert("me")} />
+    <Button text="have" oK={() => alert("have")} />
+    <Button text="nice" oK={() => alert("nice")} />
+    <Button text="day" oK={() => alert("day")} />
+    <Button text="lakhan" oK={() => alert("lakhan")} />
+    <Button text="thank " oK={() => alert("thank")} />
+    <Button text="You" oK={() => alert("You")} />
+  </div>;
 }
 
 export default App;
