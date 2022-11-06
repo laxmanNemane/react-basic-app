c55c3ba481b140edc1e9b687cb9253c220c5439e
laxmanprometteur <lnemane7@gmail.com>
2022-11-06 18:09:30 +0530

    removes unwanted file and set new folder structure

--- src/App.js
+++ src/App.js
@@ -4,14 +4,7 @@ import Button from "./Component/Button";
 function App() {
 
   return <div className="App">
-    <Button text="Click" oK={() => alert("Click")} />
-    <Button text="me" oK={() => alert("me")} />
-    <Button text="have" oK={() => alert("have")} />
-    <Button text="nice" oK={() => alert("nice")} />
-    <Button text="day" oK={() => alert("day")} />
-    <Button text="lakhan" oK={() => alert("lakhan")} />
-    <Button text="thank " oK={() => alert("thank")} />
-    <Button text="You" oK={() => alert("You")} />
+    Hello
   </div>;
 }
 
